3b6ce6b36c509ae9ba9955ccb640ef247aec5e5d
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-06-15 20:12:05 +0300

    Added AudioManager, music track, and audio settings
    - Implemented AudioManager to handle sound/music playback
    - Added sample background music
    - Customized settings for volume control

---
+++ Crystal Caverns/Crystal Caverns/Assets/Scripts/Audio Management/Sound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)] public float volume = 1f;
+    [Range (0.1f, 1f)] public float pitch = 1f;
+    public bool loop = false;
+    public bool playOnAwake = false;
+
+    [HideInInspector]
+    public AudioSource source;
+}
