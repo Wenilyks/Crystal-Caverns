a2c5fc6289a5b7f8d05b20112c45ea2a02ff66b0
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-06-28 20:23:50 +0300

    Added the ability to do a simple attack for the player.

---
+++ Crystal Caverns/Crystal Caverns/Assets/Scripts/Hero - Sam/Fireball.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private int damage = 15;
+    [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float explosionRadius = 1f;
+
+    private bool hasExploded = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (hasExploded) return;
+
+        int layer = collision.gameObject.layer;
+        if (((1 << layer) & enemyLayer) != 0 || ((1 << layer) & groundLayer) != 0)
+        {
+            Explode();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasExploded) return;
+        Explode();
+    }
+
+    private void Explode()
+    {
+        hasExploded = true;
+
+        if (explosionEffect != null)
+        {
+            GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(explosion, 2f);
+        }
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, explosionRadius, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("enemy is taking damage");
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Crystal Caverns/Crystal Caverns/Assets/Scripts/Hero - Sam/Hero-Sam.cs
+++ Crystal Caverns/Crystal Caverns/Assets/Scripts/Hero - Sam/Hero-Sam.cs
@@ -2,24 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero2 : Entity
+public enum States
+{
+    idle,
+    run,
+    jump,
+    death,
+    hit,
+    attackOne,
+    attackTwo,
+    attackThree
+}
+
+public class Hero2 : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
     [SerializeField] private int lives = 5;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform spriteHolder2; // 👉 ссылка на SpriteHolder
+    [SerializeField] private Transform spriteHolder2;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.2f;
 
+    [Header("Combat")]
+    [SerializeField] private GameObject fireballPrefab;
+    [SerializeField] private Transform fireballSpawnPoint;
+    [SerializeField] private float fireballSpeed = 8f;
+    [SerializeField] private float attackCooldown = 0.7f;
+    [SerializeField] private int rainFireballCount = 5;
+    [SerializeField] private float rainSpread = 5f;
+    [SerializeField] private float rainHeight = 3f;
+    [SerializeField] private float groundPoundRadius = 3f;
+    [SerializeField] private int groundPoundDamage = 25;
+    [SerializeField] private LayerMask enemyLayer;
+
+    [Header("Effects")]
+    [SerializeField] private GameObject goundPoundEffect;
+    [SerializeField] private ParticleSystem magicAura;
+
     private bool isGrounded = false;
+    private bool canAttack = true;
+    private bool isAttacking = false;
 
     private Rigidbody2D rb;
     private Animator anim;
-    private BoxCollider2D boxCollider;
     public static Hero2 Instance { get; set; }
-    // Значение смещения коллайдера при повороте
-    private float colliderOffsetX;
 
     private States State
     {
@@ -31,9 +58,7 @@ public class Hero2 : Entity
     {
         Instance = this;
         rb = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
-        anim = spriteHolder2.GetComponent<Animator>(); // Animator на дочернем объекте
-        colliderOffsetX = boxCollider.offset.x; // Запоминаем оригинальное смещение
+        anim = spriteHolder2.GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -43,13 +68,41 @@ public class Hero2 : Entity
 
     private void Update()
     {
-        if (isGrounded) State = States.idle;
+        if (isGrounded && !isAttacking)
+        {
+            Debug.Log("Idle state lol");
+            State = States.idle;
+        }
+        if (!isAttacking)
+        {
+            if (Input.GetButton("Horizontal"))
+                Run();
 
-        if (Input.GetButton("Horizontal"))
-            Run();
+            if (isGrounded && Input.GetButtonDown("Jump"))
+                Jump();
+        }
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
-            Jump();
+
+        if (canAttack)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                State = States.idle;
+                isAttacking = true;
+                canAttack = false;
+                State = States.attackTwo;
+                StartCoroutine(PerformAttack(States.attackTwo));
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                // Start fireball rain attack 
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                // Start ground pound attack 
+            }
+        }
+
     }
 
     private void Run()
@@ -63,17 +116,14 @@ public class Hero2 : Entity
 
         if (moveInput != 0)
         {
-            // Отражаем только спрайт
             Vector3 scale = spriteHolder2.localScale;
             scale.x = Mathf.Abs(scale.x) * (moveInput > 0 ? 1 : -1);
             spriteHolder2.localScale = scale;
-
         }
     }
 
     private void Jump()
     {
-       
         Vector2 velocity = rb.linearVelocity;
         velocity.y = 0f;
         rb.linearVelocity = velocity;
@@ -81,17 +131,78 @@ public class Hero2 : Entity
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
+    private IEnumerator PerformAttack(States attackType)
+    {
+        isAttacking = true;
+        canAttack = false;
+        if (attackType == States.attackTwo)
+        {
+            float animationLength = GetAnimationLength(attackType) / 1.3f;
+            yield return new WaitForSeconds(animationLength);
+            SpawnFireball();
+        }
 
+        isAttacking = false;
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
+
+    public void SpawnFireball()
+    {
+        if (fireballPrefab != null && fireballSpawnPoint != null)
+        {
+            GameObject fireball = Instantiate(fireballPrefab, fireballSpawnPoint.position, Quaternion.identity);
+
+            float direction = spriteHolder2.localScale.x > 0 ? 1 : -1;
+
+            fireball.transform.localScale = new Vector3(0.4f * direction, 0.4f, 0.4f);
+
+            Rigidbody2D fireballRb = fireball.GetComponent<Rigidbody2D>();
+            if (fireballRb != null)
+            {
+                fireballRb.linearVelocity = new Vector2(direction * fireballSpeed, 0);
+            }
+
+            Destroy(fireball, 5f);
+        }
+    }
+
+    private float GetAnimationLength(States attackType)
+    {
+        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+
+        string animationName = "";
+        switch (attackType)
+        {
+            case States.attackOne:
+                animationName = "attackOne"; 
+                break;
+            case States.attackTwo:
+                animationName = "attackTwo";
+                break;
+            case States.attackThree:
+                animationName = "attackThree";
+                break;
+        }
+
+        foreach (AnimationClip clip in clips)
+        {
+            if (clip.name == animationName)
+            {
+                return clip.length;
+            }
+        }
+
+        return 1f; 
+    }
 
     private void CheckGround()
-{
-    isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-    if (!isGrounded)
-        State = States.jump;
-}
-
+        if (!isGrounded && !isAttacking)
+            State = States.jump;
+    }
 
     private void OnDrawGizmosSelected()
     {
@@ -102,17 +213,9 @@ public class Hero2 : Entity
         }
     }
 
-
-    public enum States
-    {
-        idle,
-        run,
-        jump
-    }
-    public override void GetDamage()
+    public void GetDamage()
     {
         lives -= 10;
         Debug.Log(lives);
     }
-
 }
