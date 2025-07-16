9dd7c36965b48138ea0c414c06b5672ec6165d1c
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-07-16 15:07:26 +0300

    Added boss transformation feature.

---
+++ Crystal Caverns/Crystal Caverns/Assets/Scripts/Enemies/BossAbilityData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Boss Ability", menuName = "Boss Transformation/Boss Ability")]
+public class BossAbility : ScriptableObject
+{
+    public string abilityName;
+    public float magicCost;
+    public float cooldown;
+    public KeyCode activationKey;
+    public GameObject effectPrefab;
+    public float damage;
+    public float range;
+    public Sprite abilityIcon;
+    public string description;
+
+    // Animation state for this ability
+    public States animationState;
+}
